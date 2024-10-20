680446dff581927c4e4bba036c8bcae172218ca8
andan2 <epsg.ig.andrei.danciulescu@gmail.com>
2024-10-21 01:49:06 +0200

    sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+import sitemap from '@astrojs/sitemap';
 
-import robotsTxt from "astro-robots-txt"
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+  site: 'https://andreioctaviandanciulescu.com',
+  integrations: [
+    tailwind(),
+    robotsTxt({
+      policy: [
+        {
+          userAgent: '*',
+          allow: '/',
+        },
+      ],
+      sitemap: 'https://andreioctaviandanciulescu.com/sitemap-index.xml',
+    }),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
+  outDir: './dist', // Asegúrate de que esto coincida con tu directorio de salida
+});
